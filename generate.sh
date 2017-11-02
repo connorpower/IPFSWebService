4db3cb3513d50b2b4e52d3292715c6195889417b
Connor Power <connor.power@icloud.com>
2017-11-02 22:31:59 +0100

    Use swift 3 instead of 4, until the generator works better with swift 4

--- generate.sh
+++ generate.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 project_name="IPFSWebService"
-rm -rf $project_name/SwaggerClient; swagger-codegen generate -i swagger.yaml -l swift4 -o $project_name
+rm -rf $project_name/SwaggerClient; swagger-codegen generate -i swagger.yaml -l swift3 -o $project_name

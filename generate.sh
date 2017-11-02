f9f25766265e4ab17d745e66708049e23382de2e
Connor Power <connor.power@icloud.com>
2017-11-02 23:09:05 +0100

    Move all generated code one level higher

--- generate.sh
+++ generate.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-project_name="IPFSWebService"
-rm -rf $project_name/SwaggerClient; swagger-codegen generate -i swagger.yaml -l swift3 -o $project_name
+rm -rf SwaggerClient; swagger-codegen generate -i swagger.yaml -l swift3 -o ./

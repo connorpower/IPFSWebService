8f2e50cc3fd6beabddad36fc667701a26d4f7b8d
Connor Power <connor.power@icloud.com>
2017-11-02 20:44:28 +0100

    Regenerate only the generated classes, not the podscpec files

--- generate.sh
+++ generate.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 project_name="IPFS-API"
-rm -rf $project_name; swagger-codegen generate -i swagger.yaml -l swift4 -o $project_name
+rm -rf $project_name/SwaggerClient; swagger-codegen generate -i swagger.yaml -l swift4 -o $project_name

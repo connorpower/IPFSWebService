143c0b1fe13dc19be660ee8a5df0fba1d0ba8d41
Connor Power <connor.power@icloud.com>
2018-02-02 10:41:30 +0100

    Move code files into Sources/

--- generate.sh
+++ generate.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-rm -rf SwaggerClient; swagger-codegen generate -i swagger.yaml -l swift4 -o ./ --additional-properties responseAs=RxSwift
+rm -rf Sources/SwaggerClient
+swagger-codegen generate -i swagger.yaml -l swift4 -o Sources/ --additional-properties responseAs=RxSwift
+
